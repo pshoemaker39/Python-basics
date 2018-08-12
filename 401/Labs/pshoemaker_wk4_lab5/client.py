801fe23dba3d99ba57a69af413f09312b288f86f
Price Shoemaker <priceshoemaker@Prices-MacBook-Pro.local>
2018-08-12 10:33:06 -0500

    Lab 5 wip

--- 401/Labs/pshoemaker_wk4_lab5/client.py
+++ 401/Labs/pshoemaker_wk4_lab5/client.py
@@ -11,4 +11,67 @@ options = {
     '8': 'Exit'
 }
 
+def rentLeaseApartment():
+    print('Selected Rent / Lease Apartment')
+    pass
+
+def searchAvailableApartments():
+    print('Selected Search Available Apartments')
+    pass
+
+def makeApartmentAvailable():
+    print('Selected Make Apartment Available')
+    pass
+
+def listAvailableApartments():
+    print('Selected List Available Apartments')
+    pass
+
+def listRentedApartments():
+    print('Selected List Rented Apartments')
+    pass
+
+def displayTenantInformation():
+    print('Selected Display Tenant Information')
+    pass
+
+def addNewApartment():
+    print('Selected Add New Apartment')
+    pass
+
+def quitMenu():
+    print('Done')
+    quit()
+
+def optionSelector(option):
+    switcher = {
+        '1': rentLeaseApartment,
+        '2': searchAvailableApartments,
+        '3': makeApartmentAvailable,
+        '4': listAvailableApartments,
+        '5': listRentedApartments,
+        '6': displayTenantInformation,
+        '7': addNewApartment,
+        '8': quitMenu
+    }
+    switch = switcher.get(option)
+    switch()
+
+def menuController():
+
+    while True:
+
+        print('\n\nMenu options:')
+        
+        for option, title in options.items():
+            print(option, ' ', title)
+        
+        menuOption = str(input('Choice? '))
+
+        optionSelector(menuOption)
+
+        print('\n\n')
+
+
+menuController()
 
---
+++ 401/Labs/pshoemaker_wk4_lab5/tenant.py
@@ -0,0 +1,8 @@
+class Tenant:
+    def __init__(self,firstName, lastName, apartmentNumber):
+        self.tenant_fName = firstName
+        self.tenant_lName = lastName
+        self.tenant_aptNum = apartmentNumber
+
+    
+    
